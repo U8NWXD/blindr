4c911c0350fd429152d9239a4dbc25b5d9230543
cs <cs.temporary@icloud.com>
2018-06-26 17:45:57 +0000

    Add AGPL 3 License

--- autoBlind.m
+++ autoBlind.m
@@ -1,3 +1,25 @@
+% autoBlindr: A program for reversibly randomizing filenames
+% Copyright (C) 2018  U8N WXD
+
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU Affero General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU Affero General Public License for more details.
+
+% You should have received a copy of the GNU Affero General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+disp('autoBlindr  Copyright (C) 2018  U8N WXD')
+disp('This program comes with ABSOLUTELY NO WARRANTY.')
+disp('This is free software, and you are welcome to redistribute it')
+disp('under certain conditions.')
+disp('')
+
 % Constants
 EXTENSION = 'avi';
 
--- unBlind.m
+++ unBlind.m
@@ -1,3 +1,19 @@
+% autoBlindr: A program for reversibly randomizing filenames
+% Copyright (C) 2018  U8N WXD
+
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU Affero General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU Affero General Public License for more details.
+
+% You should have received a copy of the GNU Affero General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 % Constants
 KEY = 'blindingKey.csv';    % Filename of key file
 % Potentially appended to end of blinded name to create log name
